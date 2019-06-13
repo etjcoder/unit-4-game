4369f600125482dca8e082d1ce75ea431bf42655
Evan Desktop <evanjcleary@gmail.com>
2019-06-12 22:39:38 -0400

    adding first round of changes, built skeleton for html, css and js

---
+++ assets/javascript/game.js
@@ -0,0 +1,49 @@
+//Here we are going to create the Crystal Collector Game
+
+
+//We will have 6 Variables to Start with
+var computerNumber;
+var userNumber;
+var crystal1Value;
+var crystal2Value;
+var crystal3Value;
+var crystal4Value;
+
+
+// Next we will define some of the functions that we're going to need
+
+function compNumGenerator() {
+
+};
+
+function userNumberTotal () {
+
+};
+
+function crystal1NumGen () {
+
+};
+
+function crystal2NumGen () {
+
+};
+ 
+function crystal3NumGen () {
+
+};
+
+function crystal4NumGen () {
+
+};
+
+function resultDeterminer () {
+
+};
+
+function endGame () {
+
+};
+
+function reset () {
+
+};
